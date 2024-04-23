ead25773e174264a5cdbd7b72f1a02b7f14a729e
russcodes <114115059+russcodes@users.noreply.github.com>
2024-04-23 17:13:41 -0400

    Create app.jsx

---
+++ src/client/app.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Login, Room } from "./views";
+
+
+class App extends Component {
+  render() {
+    const { username, room } = this.props;
+    let body;
+    if (username) {
+      body = <Room />;
+    } else {
+      body = <Login />;
+    }
+
+    return <div>{body}</div>;
+  }
+}
+
+function select({ app }) {
+  return { ...app };
+}
+
+export default connect(select)(App);
